4aebdabf9dddd6eadd4510cc2c9636fff3a81606
imayanknegi29 <imayanknegi29@gmail.com>
2025-06-05 18:11:24 +0530

    refactor backup script

--- bash-course/scripts/backup_script.sh
+++ bash-course/scripts/backup_script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
-tar -cvf ~/Documents/bash-shell/bash-course/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/Documents/bash-shell/bash-course/* 2>/dev/null
+echo "Hello ${USER^}"
+echo "I will back up you bash-course directory, $HOME/Doucments/bash-shell/bash-course"
+
+currentdir=$(pwd)
+
+echo "You are running this script from $currentdir"
+echo "Therefore, I will save the backup in $currentdir" 
+
+tar -cf $currentdir/my_backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/Documents/bash-shell/bash-course/* 2>/dev/null
+
+echo "Backup completed successfully."
 
 exit 0
